ec377f0820d2385b2a541152d808cd5c67748f7d
FlamesX-128 <glowingorangeflames@gmail.com>
2022-07-02 23:36:24 -0500

    data type changes.

--- types/main.go
+++ types/main.go
@@ -1,4 +1,4 @@
-package types
+package main
 
 //
 
@@ -36,4 +36,19 @@ type PluginType interface {
 type Plugin[T PluginType] struct {
 	Author, Name, Url, Version string
 	This                       T
+}
+
+//
+
+type Anime struct {
+	Image, Name, Url string
+}
+
+//
+
+type SliceContent interface {
+	complex64 | complex128 | float32 | float64 |
+		uint8 | uint16 | uint32 | uint | uintptr |
+		int8 | int16 | int32 | int |
+		bool | string
 }

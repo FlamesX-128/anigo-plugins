83cc4cd6957b0d9a5665f216a07d9e5e5ec2924d
FlamesX-128 <glowingorangeflames@gmail.com>
2022-07-03 00:42:30 -0500

    rich-presence: first commit

---
+++ rich-presence/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"log"
+	"reflect"
+	"time"
+
+	"github.com/FlamesX-128/anigo-plugins/types"
+	"github.com/hugolgst/rich-go/client"
+)
+
+var Buttons = []*client.Button{
+	{
+		Url:   "https://github.com/FlamesX-128/anigo",
+		Label: "GitHub",
+	},
+}
+
+func checkVariables(a *types.Anigo) bool {
+	refl := reflect.ValueOf(a)
+
+	for _, e := range []string{"Episode", "Image", "Name", "Url"} {
+		if refl.FieldByName(e).String() == "" {
+
+			return false
+		}
+
+	}
+
+	return true
+}
+
+func Handler(a *types.Anigo) {
+	if err := client.Login("940982082891579482"); err != nil {
+		log.Println(err)
+
+		return
+	}
+
+	var now = time.Now()
+
+	for i := 0; i < 4; {
+		time.Sleep(time.Second * 1)
+
+		if !checkVariables(a) {
+			now = time.Now()
+
+		}
+
+		err := client.SetActivity(client.Activity{
+			State:      "Watching episode " + string(a.Episode),
+			Details:    a.Anime,
+			LargeImage: a.Image,
+			LargeText:  a.Anime,
+			Timestamps: &client.Timestamps{
+				Start: &now,
+			},
+			Buttons: Buttons,
+		})
+
+		if i == 3 {
+			log.Println(err)
+
+			break
+		}
+
+		if err != nil {
+			i++
+
+		}
+
+	}
+
+}
+
+var Plugin = types.Plugin[types.Process]{
+	Author: "FlamesX-128",
+	Name:   "rich-presence",
+	This: types.Process{
+		Handler:    Handler,
+		Persistent: false,
+	},
+	Url:     "github.com/FlamesX-128/anigo-plugins/rich-presence",
+	Version: "0.1.0",
+}

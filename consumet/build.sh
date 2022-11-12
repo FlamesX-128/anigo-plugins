42fdaab7ff338a06d24abc04e6aa128c2014b9e2
FlamesX-128 <flamesx128@gmail.com>
2022-11-11 22:57:02 -0600

    fixing build script.

--- consumet/build.sh
+++ consumet/build.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/sh
 
-VERSION='1.0.0'
-NAME='consument'
-
-go build -buildmode=plugin -o dist/linux-amd64/consument.so main.go
+go build -buildmode=plugin -o dist/linux-amd64/consumet.so main.go
